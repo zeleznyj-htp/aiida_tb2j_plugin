e20520179e213ecc997e260c89fcc752a2316ffa
antelmor <andres.tellez.m13@gmail.com>
2024-11-19 17:03:25 -0500

    Updated Merger to match the new merger in TB2J

--- aiida_tb2j/utils/merger.py
+++ aiida_tb2j/utils/merger.py
@@ -2,7 +2,7 @@ import pickle
 from TB2J.io_merge import Merger as TB2JMerger
 from TB2J.io_merge import SpinIO_merge
 
-def read_pickle(folder_data, projv_tolerance=1e-2):
+def read_pickle(folder_data):
 
     try:
         filename = [element for element in folder_data.list_object_names() if '.pickle' in element][0]
@@ -14,23 +14,22 @@ def read_pickle(folder_data, projv_tolerance=1e-2):
 
     spin_obj = SpinIO_merge(atoms=[], spinat=[], charges=[], index_spin=[])
     spin_obj.__dict__.update(content)
-    spin_obj._set_projection_vectors(projv_tolerance=projv_tolerance)
+    spin_obj._build_Rlist()
+    spin_obj._set_projection_vectors()
 
     return spin_obj
 
 class Merger(TB2JMerger):
 
-    def __init__(self, *folders, main_folder=None, method='structure', tolerance=1e-2):
+    def __init__(self, *folders, main_folder=None):
         from copy import deepcopy
-        if method not in ['structure', 'spin']:
-            raise ValueError(f"Unrecognized method '{method}'. Available options are: 'structure' or 'spin'.")
-        self.method = method
-        self.dat = [read_pickle(folder, projv_tolerance=tolerance) for folder in folders]
+
+        self.dat = [read_pickle(folder) for folder in folders]
 
         if main_folder is None:
             self.main_dat = deepcopy(self.dat[-1])
         else:
-            self.main_dat = read_pickle(main_folder, projv_tolerance=tolerance)
+            self.main_dat = read_pickle(main_folder)
             self.dat.append(deepcopy(self.main_dat))
 
         self._set_projv()
